b265bf460a2902c7b54b4eb93704861980d64d8d
MyAeroCode <meteo_meteo@naver.com>
2020-09-08 18:48:33 +0900

    add: 테스트케이스 및 솔루션 함수 표준 템플릿

---
+++ src/solution.ts
@@ -0,0 +1,23 @@
+//
+// 표준입력에서 데이터 읽기
+import readline from "readline";
+import fs from "fs";
+import { argv } from "process";
+const lines = [] as string[];
+if (argv.includes("readFromInputFile")) {
+    lines.push(...fs.readFileSync("./src/stdin").toString().split("\n"));
+    solution();
+} else {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.on("line", (line) => lines.push(line.trim())).on("close", solution);
+}
+
+//
+// 솔루션 함수
+function solution() {
+    //
+    // 여기에서 정답을 출력해주세요.
+}
