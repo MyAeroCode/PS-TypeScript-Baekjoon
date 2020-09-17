0c22c49008ebcef45666512c90eebbd7cdb8900b
MyAeroCode <meteo_meteo@naver.com>
2020-09-17 16:18:05 +0900

    add: 전화번호 목록

---
+++ problem/5052/solution.ts
@@ -0,0 +1,162 @@
+function solution(io: NodeIO): void {
+    class Trie {
+        private data = new Map<string, Trie>();
+        private size = 0;
+
+        has([first, ...others]: string[]): boolean {
+            if (first === undefined) throw new Error();
+            if (others.length === 0) return this.data.has(first);
+            return this.of(first).has(others);
+        }
+
+        of(elem: string): Trie {
+            let childTrie = this.data.get(elem);
+            if (childTrie === undefined) {
+                childTrie = new Trie();
+                this.data.set(elem, childTrie);
+            }
+            return childTrie;
+        }
+
+        insert([first, ...others]: string[]): void {
+            if (first === undefined) return;
+            const childTrie = this.of(first);
+            childTrie.insert(others);
+            childTrie.size++;
+        }
+
+        getSize(): number {
+            return this.size;
+        }
+    }
+
+    makeArray(io.readInt(), () => {
+        const trieRoot = new Trie();
+        const phoneNums = makeArray(io.readInt(), () => io.readLine())
+            .sort()
+            .reverse();
+        for (const phoneNum of phoneNums) {
+            const elems = phoneNum.split("");
+            if (trieRoot.has(elems)) {
+                return false;
+            }
+            trieRoot.insert(elems);
+        }
+        return true;
+    })
+        .map((result) => (result ? "YES" : "NO"))
+        .forEach((v) => io.write(v));
+}
+//
+// 입출력 객체
+class NodeIO {
+    private debug: boolean = process.argv.includes("readFromInputFile");
+    private buffer: Buffer = require("fs").readFileSync(this.debug ? "./src/stdin" : 0);
+    private stdout: any[] = [];
+    private cursor: number = 0;
+    constructor() {
+        this.consumeWhiteSpace = this.consumeWhiteSpace.bind(this);
+        this.readInt = this.readInt.bind(this);
+        this.readLine = this.readLine.bind(this);
+        this.readWord = this.readWord.bind(this);
+        this.write = this.write.bind(this);
+        this.flush = this.flush.bind(this);
+    }
+    /**
+     * 공백이 아닌 문자를 만날 때 까지 전진한다.
+     */
+    private consumeWhiteSpace(): void {
+        const { buffer } = this;
+        while (true) {
+            const char = buffer[this.cursor];
+            if (char === 10 || char === 13 || char === 32) ++this.cursor;
+            else break;
+        }
+    }
+    /**
+     * 소수점 없는 부호화된 정수 하나를 읽는다.
+     */
+    public readInt(): number {
+        const { buffer } = this;
+        this.consumeWhiteSpace();
+        const isNegative = buffer[this.cursor] === 45;
+        if (isNegative) ++this.cursor;
+        let num = 0;
+        while (true) {
+            const char = buffer[this.cursor];
+            if (48 <= char && char <= 57) {
+                num = num * 10 + char - 48;
+                ++this.cursor;
+            } else break;
+        }
+        return isNegative ? -num : num;
+    }
+    /**
+     * 단어 하나를 읽는다.
+     */
+    public readWord(): string {
+        const { buffer } = this;
+        this.consumeWhiteSpace();
+        const srt = this.cursor;
+        while (true) {
+            const char = buffer[this.cursor];
+            if (char === 10 || char === 13 || char === 32 || char === undefined) break;
+            else ++this.cursor;
+        }
+        return this.buffer.slice(srt, this.cursor).toString("utf8");
+    }
+    /**
+     * 라인 하나를 읽는다.
+     */
+    public readLine(): string {
+        const { buffer } = this;
+        this.consumeWhiteSpace();
+        const srt = this.cursor;
+        while (true) {
+            const char = buffer[this.cursor];
+            if (char === 10 || char === 13 || char === undefined) break;
+            else ++this.cursor;
+        }
+        return this.buffer.slice(srt, this.cursor).toString("utf8");
+    }
+    /**
+     * 메세지를 콘솔에 적는다.
+     * 채점 모드에서는 메세지를 버퍼에 삽입한다.
+     */
+    public write(message: any): void {
+        if (this.debug) {
+            console.log(message);
+        } else {
+            this.stdout.push(message);
+        }
+    }
+    /**
+     * 버퍼에 삽입된 메세지를 한꺼번에 출력하고, 프로그램을 종료한다.
+     */
+    public flush(): void {
+        if (this.debug) {
+            //
+        } else {
+            console.log(this.stdout.join("\n"));
+            process.exit(0);
+        }
+    }
+}
+const nodeIO = new NodeIO();
+solution(nodeIO);
+nodeIO.flush();
+//
+// utils
+interface makeArray {
+    (size: number): number[];
+    <T>(size: number, initializer: (idx: number) => T): T[];
+}
+function makeArray<T>(size: number, initializer: (idx: number) => T): T[];
+function makeArray<T>(size: number): number[];
+function makeArray<T>(size: number, initializer?: (idx: number) => T): T[] | number[] {
+    const array: any[] = [];
+    for (let i = 0; i < size; i++) {
+        array.push(initializer ? initializer(i) : i);
+    }
+    return array;
+}
